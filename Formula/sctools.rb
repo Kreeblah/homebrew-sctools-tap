20de01501562c5ab272372262a45a04f6bda9005
Kreeblah <kreeblah@gmail.com>
2024-02-03 10:09:43 -0800

    Update to 1.14.3

--- Formula/sctools.rb
+++ Formula/sctools.rb
@@ -1,8 +1,8 @@
 class Sctools < Formula
   desc "Tools for Soarer's Converter"
   homepage "https://github.com/Kreeblah/sctools_macos"
-  url "https://github.com/Kreeblah/sctools_macos/archive/v1.14.2.tar.gz"
-  sha256 "5af48cbbe03a3b77c1e5f00c47085e7624c96329f5279868e0af35fe7d5a89cd"
+  url "https://github.com/Kreeblah/sctools_macos/archive/v1.14.3.tar.gz"
+  sha256 "fada780cae674c4cca1a55f923aecd7df83ccf61a4fde71981d5b6b1ef5d29eb"
 
   def install
     arch = Hardware::CPU.arm? ? "arm64" : "x86_64"
